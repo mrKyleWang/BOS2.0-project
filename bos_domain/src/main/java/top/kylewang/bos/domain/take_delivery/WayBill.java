fdd9ef0e9f402c83d00c9dd7454cb51d2ed6caac
KyleWang <Kyle.Wang.cn@outlook.com>
2018-01-09 22:35:01 +0800

    为WayBill加elasticsearch文档对象注解

--- bos_domain/src/main/java/top/kylewang/bos/domain/take_delivery/WayBill.java
+++ bos_domain/src/main/java/top/kylewang/bos/domain/take_delivery/WayBill.java
@@ -1,5 +1,9 @@
 package top.kylewang.bos.domain.take_delivery;
 
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldIndex;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 import top.kylewang.bos.domain.base.Area;
 
 import javax.persistence.*;
@@ -9,65 +13,90 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name = "T_WAY_BILL")
+@Document(indexName = "bos",type = "waybill")
 public class WayBill {
 
 	@Id
 	@GeneratedValue
 	@Column(name = "C_ID")
+	@org.springframework.data.annotation.Id
+	@Field(index = FieldIndex.not_analyzed,store = true,type = FieldType.Integer)
 	private Integer id;
+
 	@Column(name = "C_WAY_BILL_NUM", unique = true)
+	@Field(index = FieldIndex.not_analyzed,store = true,type = FieldType.String)
 	private String wayBillNum; // 运单编号
+
 	@OneToOne
 	@JoinColumn(name = "C_ORDER_ID")
 	private Order order; // 订单信息
 
 	@Column(name = "C_SEND_NAME")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String sendName; // 寄件人姓名
 	@Column(name = "C_SEND_MOBILE")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String sendMobile;// 寄件人电话
 	@Column(name = "C_SEND_COMPANY")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String sendCompany;// 寄件人公司
 	@OneToOne
 	@JoinColumn(name = "C_SEND_AREA_ID")
 	private Area sendArea; // 寄件人省市区信息
 	@Column(name = "C_SEND_ADDRESS")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String sendAddress;// 寄件人详细地址信息
 
 	@Column(name = "C_REC_NAME")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String recName;// 收件人姓名
 	@Column(name = "C_REC_MOBILE")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String recMobile;// 收件人电话
 	@Column(name = "C_REC_COMPANY")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String recCompany;// 收件人公司
 	@OneToOne
 	@JoinColumn(name = "C_REC_AREA_ID")
 	private Area recArea; // 收件人省市区信息
 	@Column(name = "C_REC_ADDRESS")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String recAddress;// 收件人详细地址信息
 
 	@Column(name = "C_SEND_PRO_NUM")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String sendProNum; // 快递产品类型编号：速运当日、速运次日、速运隔日
 	@Column(name = "C_GOODS_TYPE")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String goodsType;// 托寄物类型：文件、衣服 、食品、电子商品
 	@Column(name = "C_PAY_TYPE_NUM")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String payTypeNum;// 支付类型编号：寄付日结、寄付月结、到付
 	@Column(name = "C_WEIGHT")
+	@Field(index = FieldIndex.no,store = true,type = FieldType.String)
 	private Double weight;// 托寄物重量
 	@Column(name = "C_REMARK")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String remark; // 备注
 	@Column(name = "C_NUM")
+	@Field(index = FieldIndex.no,store = true,type = FieldType.String)
 	private Integer num; // 原件数
 
 	@Column(name = "C_ARRIVE_CITY")
+	@Field(index = FieldIndex.analyzed,analyzer = "ik",searchAnalyzer = "ik",store = true,type = FieldType.String)
 	private String arriveCity; // 到达地
 
 	@Column(name = "C_FEEITEMNUM")
+	@Field(index = FieldIndex.no,store = true,type = FieldType.String)
 	private Integer feeitemnum; // 实际件数
 	@Column(name = "C_ACTLWEIT")
+	@Field(index = FieldIndex.no,store = true,type = FieldType.String)
 	private Double actlweit; // 实际重量
 	@Column(name = "C_VOL")
+	@Field(index = FieldIndex.no,store = true,type = FieldType.String)
 	private String vol; // 体积 输入格式为1*1*1*1，表示长*宽*高*数量
 	@Column(name = "C_FLOADREQR")
+	@Field(index = FieldIndex.no,store = true,type = FieldType.String)
 	private String floadreqr; // 配载要求 (比如录入1=无，2=禁航，4=禁航空铁路)
 
 	@Column(name = "C_WAY_BILL_TYPE")
@@ -76,6 +105,7 @@ public class WayBill {
 	 * 运单状态： 1 待发货、 2 派送中、3 已签收、4 异常
 	 */
 	@Column(name = "C_SIGN_STATUS")
+	@Field(index = FieldIndex.not_analyzed,store = true,type = FieldType.String)
 	private Integer signStatus; // 签收状态
 
 	/*
